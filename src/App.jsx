f7c98995def6fdb514eec8e197550934bebcad92
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-14 14:12:40 +0600

    added google font

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      <h1>The Daily Insight</h1>
+      <h1 className="text-3xl font-black">The Daily Insight</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
