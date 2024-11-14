a99f402792442aac9e8a8c42017cd6a7168257cd
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-14 18:41:11 +0600

    added dropdown menu in navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,38 @@
 import { NavLink } from "react-router-dom";
 
-
 const Navbar = () => {
-    return (
-      <div>
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            
-          </div>
-          
-          <div className="navbar-end">
-            <a className="btn">Button</a>
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start"></div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal  flex items-center gap-8 font-bold text-lg text-slate-600">
+            <NavLink to={"/home"}>Home</NavLink>
+            <NavLink to={"/about"}>About</NavLink>
+            <NavLink to={"/career"}>Career</NavLink>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-bottom dropdown-end">
+            <div tabIndex={0} role="button" className="btn m-1">
+              Click
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
