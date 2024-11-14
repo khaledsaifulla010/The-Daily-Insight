834905b95f00489c2a23aac0cc2cddb74f14f8d3
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-14 18:13:07 +0600

    added time & date functionality in header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
+import moment from "moment";
 import logo from "../../assets/logo.jpg";
 
 const Header = () => {
   return (
     <div>
       <div className="flex flex-col justify-center items-center mt-12">
-        <img className="w-[350px]" src={logo} />
-        <h1 className="text-base font-black text-slate-600">
+        <img className="w-[420px]" src={logo} />
+        <h1 className="text-lg font-black text-slate-600">
           Journalism Without Fear or Favour
         </h1>
+        <p className="font-semibold text-slate-700">
+          {moment().format("dddd, MMMM Do YYYY")}
+        </p>
       </div>
     </div>
   );
