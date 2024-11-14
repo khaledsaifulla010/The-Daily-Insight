11099002c285a43dfa85d17577e631dcf1de84bd
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-14 19:36:53 +0600

    added find us on section

--- src/layouts/LayoutComponents/FindUs/FindUs.jsx
+++ src/layouts/LayoutComponents/FindUs/FindUs.jsx
@@ -1,11 +1,25 @@
-
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const FindUs = () => {
-    return (
-      <div>
-  
+  return (
+    <div className="mt-16">
+      <h1 className="font-black text-xl">Find Us On</h1>
+      <div className="grid gap-4 mt-4">
+        <button className="border-2 p-2 rounded-xl flex items-center gap-2 font-semibold w-[170px]">
+          <FaFacebook className="ml-6"></FaFacebook>
+          Facebook
+        </button>
+        <button className="border-2 p-2 rounded-xl flex items-center gap-2 font-semibold w-[170px]">
+          <FaTwitter className="ml-6"></FaTwitter>
+          Twitter
+        </button>
+        <button className="border-2 p-2 rounded-xl flex items-center gap-2 font-semibold w-[170px]">
+          <FaInstagram className="ml-6"></FaInstagram>
+          Instagram
+        </button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default FindUs;
--- src/layouts/LayoutComponents/RightNavbar/RightNavbar.jsx
+++ src/layouts/LayoutComponents/RightNavbar/RightNavbar.jsx
@@ -1,9 +1,11 @@
+import FindUs from "../FindUs/FindUs";
 import SocialLogin from "../SocialLogin/SocialLogin";
 
 const RightNavbar = () => {
   return (
     <div>
       <SocialLogin></SocialLogin>
+      <FindUs></FindUs>
     </div>
   );
 };
