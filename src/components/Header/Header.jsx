3a1c92b1cb4aa5f265249fab507b356b0a82d356
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-14 18:06:52 +0600

    added logo & subtitle in header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-
+import logo from "../../assets/logo.jpg";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center mt-12">
+        <img className="w-[350px]" src={logo} />
+        <h1 className="text-base font-black text-slate-600">
+          Journalism Without Fear or Favour
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
